f9cedf7387ff2b4deff67b70ebd7e64d8195cb8f
barjomet <barjomet@barjomet.com>
2014-08-13 00:20:49 +0300

    ticket:137

--- wallet/src/de/schildbach/wallet/litecoin/Constants.java
+++ wallet/src/de/schildbach/wallet/litecoin/Constants.java
@@ -72,8 +72,8 @@ public class Constants
 	public static final String PEER_DISCOVERY_IRC_CHANNEL_PROD = "#lycancoin";
 	public static final String PEER_DISCOVERY_IRC_CHANNEL_TEST = "#lycancoinTEST3";
 
-	private static final String BLOCKEXPLORER_BASE_URL_PROD = "http://explorer.lyccoin.org/";
-	private static final String BLOCKEXPLORER_BASE_URL_TEST = "http://explorer.lyccoin.org/";
+	private static final String BLOCKEXPLORER_BASE_URL_PROD = "http://explorer.lyccoin.org/index.php?block_hash=";
+	private static final String BLOCKEXPLORER_BASE_URL_TEST = "http://explorer.lyccoin.org/index.php?block_hash=";
 	public static final String BLOCKEXPLORER_BASE_URL = TEST ? BLOCKEXPLORER_BASE_URL_TEST : BLOCKEXPLORER_BASE_URL_PROD;
 
 	public static final String MIMETYPE_TRANSACTION = "application/x-lyctx";
